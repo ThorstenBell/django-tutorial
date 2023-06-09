07c0156acab67bd2b72a45dba559527328ceb6f6
ThorstenBell <thorst@gmail.com>
2023-06-09 22:12:12 +0200

    Almost end of section 6

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # django-tutorial
  https://www.udemy.com/course/python-django-the-practical-guide/
+
+# Run server
+
+```sh
+py .\manage.py runserver
+```
+
+### PyCharm Django formater
+- Ctrl + Alt + S
+- Languages & Frameworks
+- Django
+  - Set project root
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,15 +1,48 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+monthly_challenges = {
+    "january": "Read a book",
+    "february": "Workout six times a week",
+    "march": "Have a cold shower everyday",
+    "april": "Have a cold shower everyday",
+    "may": "Have a cold shower everyday",
+    "june": "Have a cold shower everyday",
+    "july": "Have a cold shower everyday",
+    "august": "Have a cold shower everyday",
+    "september": "Have a cold shower everyday",
+    "october": "Have a cold shower everyday",
+    "november": "Have a cold shower everyday",
+    "december": None,
+}
+
+
+def index(request):
+    months = list(monthly_challenges.keys())
+
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
 
 
 # Create your views here.
-def monthly_challenges(request, month):
-    match month:
-        case "january":
-            challenge_text = "Read a book"
-        case "february":
-            challenge_text = "Workout 6 times a week"
-        case other:
-            return HttpResponseNotFound("No challenge found!")
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return render(request, "challenges/challenge.html", {
+            "month_name": month,
+            "text": challenge_text,
+        })
+    except:
+        raise Http404()
 
-    return HttpResponse(challenge_text)
+
+# Create your views here.
+def monthly_challenge_by_number(request, month):
+    months = list(monthly_challenges.keys())
+    if month > len(months) or month <= 0:
+        raise Http404()
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+    return HttpResponseRedirect(redirect_path)
